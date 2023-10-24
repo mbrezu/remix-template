8c506790629d6e0d614027f2ff806d7a12873191
Miron Brezuleanu <mbrezu@gmail.com>
2023-10-24 18:54:45 +0300

    Fix tests.

--- tests/Header.test.tsx
+++ tests/Header.test.tsx
@@ -1,9 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../app/components/Header';
 import '@testing-library/jest-dom/';
+import { createRemixStub } from "@remix-run/testing";
 
 it("includes hello world", () => {
-    render(<Header/>);
+    let RemixStub = createRemixStub([
+        {
+            path: "/",
+            Component: Header,
+        }
+    ]);
+
+    render(<RemixStub />);
     const helloWorld = screen.getByText(/hello world/i);
     expect(helloWorld).toBeInTheDocument();
 });
